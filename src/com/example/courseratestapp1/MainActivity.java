7b48f5b1166ff244564f21418ebc0f55e54d356e
dilip kushwaha <kush.dilip@gmail.com>
2013-03-11 18:53:58 +0530

    File download worked.next step parsing that file

--- src/com/example/courseratestapp1/MainActivity.java
+++ src/com/example/courseratestapp1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.courseratestapp1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,16 +11,15 @@ import java.nio.charset.Charset;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.DownloadManager;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 
@@ -83,24 +84,54 @@ public class MainActivity extends Activity {
 		protected Void doInBackground(Void... params) {
 			StringBuilder builder = new StringBuilder();
 			
-//			String url = "https://www.coursera.org/maestro/api/topic/list?full=1";
-			String url = "https://www.coursera.org/maestro/api/university/list";
+			String url = "https://www.coursera.org/maestro/api/topic/list?full=1";
+//			String url = "https://www.coursera.org/maestro/api/university/list";
 			//String url = "http://search.twitter.com/search.json?q=javacodegeeks";
 			
-			DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-			
-			
 			try {
+
+				
 				InputStream source = ma.retrieveStream(url);
 
 				BufferedReader reader = new BufferedReader(
 						new InputStreamReader(source, Charset.forName("UTF-8")));
 
 				int cp, count = 0;
-				while ((cp = reader.read()) != -1) {
-					builder.append((char) cp);
-					count++;
-				}
+//				while ((cp = reader.read()) != -1) {
+//					builder.append((char) cp);
+//					count++;
+//				}
+				
+				HttpGet httpGet = new HttpGet(url);
+				HttpResponse response = client.execute(httpGet);
+				StatusLine statusLine = response.getStatusLine();
+				int statusCode = statusLine.getStatusCode();
+				
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				
+				String PATH = Environment.getExternalStorageDirectory() + "/download/";
+				Log.v(ma.getClass().getName(), "PATH: " + PATH);
+				File file = new File(PATH);
+				file.mkdirs();
+				
+				String fileName = "list2.json";
+				
+				File outputFile = new File(file,fileName);
+				FileOutputStream fos = new FileOutputStream(outputFile);
+				
+				InputStream is = content;
+				
+				byte[] buffer = new byte[1024];
+	            int len1 = 0;
+	            while ((len1 = is.read(buffer)) != -1) {
+
+	                fos.write(buffer, 0, len1);
+
+	            }
+	            fos.close();
+	            is.close();
+				
 				
 				String jsonText = builder.toString();
 				
@@ -111,7 +142,7 @@ public class MainActivity extends Activity {
 				
 
 			} catch (Exception e) {
-				// TODO: handle exception
+				e.printStackTrace();
 			}
 
 			return null;
